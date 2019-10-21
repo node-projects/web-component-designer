c628b14c2dba65c6b1269369f26b9dc2bd5bc221
jkuehner <jochen.kuehner@kardex.com>
2019-10-21 15:05:08 +0200

    use lit element

--- src/elements/demo-view.ts
+++ src/elements/demo-view.ts
@@ -1,47 +1,56 @@
-import { PolymerElement } from '@polymer/polymer/polymer-element.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
-import { customElement } from '@polymer/decorators';
+import { customElement, LitElement, html, css } from 'lit-element';
 
 @customElement('demo-view')
-export class DemoView extends PolymerElement {
-  static get template() {
+export class DemoView extends LitElement {
+
+  _placeholder: HTMLDivElement;
+  _loading: HTMLDivElement;
+
+  static get styles() {
+    return css`:host {
+      display: block;
+      overflow: hidden;
+      height: 100%;
+      width: 100%;
+    }
+    #placeholder {
+      height: 100%;
+      width: 100%;
+    }
+    #loading {
+      position: absolute;
+      top: 60px;
+      left: 20px;
+    }
+    iframe {
+      width: 100%;
+      height: 100%;
+    }`;
+  }
+
+  render() {
     return html`
-      <style>
-        :host {
-          display: block;
-          overflow: hidden;
-          height: 100%;
-          width: 100%;
-        }
-        #placeholder {
-          height: 100%;
-          width: 100%;
-        }
-        #loading {
-          position: absolute;
-          top: 60px;
-          left: 20px;
-        }
-        iframe {
-          width: 100%;
-          height: 100%;
-        }
-      </style>
       <div id="placeholder"></div>
       <div id="loading">🛀 Hold on, loading...</div>
     `;
   }
 
+  firstUpdated() {
+    this._placeholder = this.shadowRoot.getElementById('placeholder') as HTMLDivElement;
+    this._loading = this.shadowRoot.getElementById('loading') as HTMLDivElement;
+  }
+
   display(code) {
+
     let iframe = document.createElement('iframe');
     iframe.frameBorder = '0';
-    this.$.placeholder.innerHTML = '';
-    this.$.placeholder.appendChild(iframe);
-    (this.$.loading as HTMLDivElement).hidden = false;
+    this._placeholder.innerHTML = '';
+    this._placeholder.appendChild(iframe);
+    this._loading.hidden = false;
 
-    iframe.onload = function(this: DemoView) {
-      (this.$.loading as HTMLDivElement).hidden = true;
-    }.bind(this);
+    iframe.onload = () => {
+      this._loading .hidden = true;
+    };
 
     let doc = iframe.contentWindow.document;
     doc.open();
