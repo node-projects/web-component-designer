6b3775542b8ec3670b4d12c363fe7b3e2ad20673
jkuehner <jochen.kuehner@kardex.com>
2020-03-18 14:46:03 +0100

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,37 @@ export * from "./elements/services/elementsService/JsonElementsService.js";
 
 export * from "./elements/services/instanceService/DefaultInstanceService.js";
 export type { IInstanceService } from "./elements/services/instanceService/IInstanceService.js";
+
+export * from "./elements/services/propertiesService/CssPropertiesService.js";
+export * from "./elements/services/propertiesService/DefaultEditorTypesService.js";
+export type { IEditorTypesService } from "./elements/services/propertiesService/IEditorTypesService.js";
+export type { IPropertiesService } from "./elements/services/propertiesService/IPropertiesService.js";
+export type { IProperty } from "./elements/services/propertiesService/IProperty.js";
+export * from "./elements/services/propertiesService/LitElementPropertiesService.js";
+export * from "./elements/services/propertiesService/NativeElementsPropertiesService.js";
+export * from "./elements/services/propertiesService/PolymerPropertiesService.js";
+
+export type { ISelectionChangedEvent } from "./elements/services/selectionService/ISelectionChangedEvent.js";
+export type { ISelectionService } from "./elements/services/selectionService/ISelectionService.js";
+export * from "./elements/services/selectionService/SelectionService.js";
+
+export * from "./elements/services/undoService/ChangeGroup.js";
+export type { IUndoItem } from "./elements/services/undoService/IUndoItem.js";
+export type { IUndoService } from "./elements/services/undoService/IUndoService.js";
+export * from "./elements/services/undoService/UndoItemType.js";
+export * from "./elements/services/undoService/UndoService.js";
+
+export * from "./elements/services/BaseServiceContainer.js";
+export * from "./elements/services/InstanceServiceContainer.js";
+export type { IService } from "./elements/services/IService.js";
+export type { IServiceContainer } from "./elements/services/IServiceContainer.js";
+export * from "./elements/services/ServiceContainer.js";
+
+export * from "./elements/attributeEditor.js";
+export * from "./elements/attributeEditorAttributeList.js";
+export * from "./elements/canvasView.js";
+export * from "./elements/code-view-ace.js";
+export * from "./elements/demoView.js";
+export * from "./elements/paletteElements.js";
+export * from "./elements/paletteView.js";
+export * from "./elements/treeView.js";
