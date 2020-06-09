e46703d759456c130d84eff174df6dc86f98c80e
jkuehner <jochen.kuehner@kardex.com>
2020-06-09 22:14:14 +0200

    fix import

--- src/elements/widgets/designerView/designerView.ts
+++ src/elements/widgets/designerView/designerView.ts
@@ -20,7 +20,7 @@ import { IDesignerView } from './IDesignerView';
 import { Snaplines } from './Snaplines';
 import { IDesignerMousePoint } from '../../../interfaces/IDesignerMousePoint';
 import { ContextMenuHelper } from '../../helper/contextMenu/ContextMenuHelper';
-import { IPlacementView } from './IPlacementView';import { DomHelper } from '../../helper/DomHelper';
+import { IPlacementView } from './IPlacementView';
 
 export class DesignerView extends BaseCustomWebComponent implements IDesignerView, IPlacementView {
   // Public Properties
