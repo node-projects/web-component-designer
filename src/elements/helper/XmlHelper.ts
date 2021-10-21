9e7bd48405ee2ac1a8239f30d13a1b699a5f5ca6
yluithle <yannis.luithle@kardex.com>
2021-10-21 13:38:36 +0200

    fix attribute parsing

---
+++ src/elements/helper/XmlHelper.ts
@@ -0,0 +1,15 @@
+export function encodeXMLChars(value: string) {
+    return value.replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&apos;');
+};
+
+export function decodeXMLChars(value: string) {
+    return value.replace(/&apos;/g, "'")
+        .replace(/&quot;/g, '"')
+        .replace(/&gt;/g, '>')
+        .replace(/&lt;/g, '<')
+        .replace(/&amp;/g, '&');
+};
