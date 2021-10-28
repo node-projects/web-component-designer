3dc440593edaa1b1c6333dca7f0589e9b5f39631
Jochen Kühner <jochen.kuehner@gmx.de>
2021-10-28 10:22:23 +0200

    pickcolor tool

--- src/elements/widgets/designerView/tools/PickColorTool.ts
+++ src/elements/widgets/designerView/tools/PickColorTool.ts
@@ -1,12 +1,26 @@
+import { EventNames } from '../../../../enums/EventNames.js';
 import { IDesignerCanvas } from '../IDesignerCanvas';
 import { ITool } from './ITool';
 
 export class PickColorTool implements ITool {
   cursor: string;
-  
-  pointerEventHandler(designerView: IDesignerCanvas, event: PointerEvent, currentElement: Element) {
+
+  async pointerEventHandler(designerCanvas: IDesignerCanvas, event: PointerEvent, currentElement: Element) {
+    if (event.type == EventNames.PointerDown) {
+      //@ts-ignore
+      const eyeDropper = new EyeDropper();
+      const colorSelectionResult = await eyeDropper.open();
+      const color = colorSelectionResult.sRGBHex;
+      
+      if (event.button == 2)
+        designerCanvas.serviceContainer.globalContext.fillBrush = color;
+      else
+        designerCanvas.serviceContainer.globalContext.strokeColor = color;
+
+      designerCanvas.serviceContainer.globalContext.finishedWithTool(this);
+    }
   }
-  
+
   dispose(): void {
   }
 }
