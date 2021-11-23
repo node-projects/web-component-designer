7f0539a9c615c3f6d28e2c80a18e1b8ae8b7148c
Jochen Kühner <jochen.kuehner@gmx.de>
2021-11-23 01:11:00 +0100

    bugfix designer tab control

--- src/elements/controls/DesignerTabControl.ts
+++ src/elements/controls/DesignerTabControl.ts
@@ -22,6 +22,7 @@ export class DesignerTabControl extends BaseCustomWebComponentLazyAppend {
             display: flex; 
             flex-direction: column; 
             height: 100%;
+            position: relative;
         }
         .header {
             display: inline-flex; 
