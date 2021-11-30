7745d3a23b11ee92b133c1190c50018f1b3f89ed
kkranich <kai.kranich@kardex.com>
2021-11-30 17:46:07 +0100

    adding new modes to the DrawPathTool

--- src/elements/widgets/designerView/tools/DrawPathTool.ts
+++ src/elements/widgets/designerView/tools/DrawPathTool.ts
@@ -6,6 +6,13 @@ import { ITool } from './ITool';
 import { DesignItem } from '../../../item/DesignItem';
 import { OverlayLayer } from '../extensions/OverlayLayer.js';
 import { ServiceContainer } from '../../../services/ServiceContainer.js';
+import { IPoint } from '../../../..';
+
+let lastPoint: IPoint = { x: 0, y: 0 };
+let samePoint = false;
+let p2pMode = false;
+let pointerMoved = false;
+let eventStarted = false;
 
 export class DrawPathTool implements ITool {
 
@@ -13,7 +20,7 @@ export class DrawPathTool implements ITool {
 
   private _pathD: string;
   private _path: SVGPathElement;
-  
+
   constructor() {
   }
 
@@ -25,11 +32,13 @@ export class DrawPathTool implements ITool {
 
   pointerEventHandler(designerCanvas: IDesignerCanvas, event: PointerEvent, currentElement: Element) {
     const currentPoint = designerCanvas.getNormalizedEventCoordinates(event);
-
     const offset = 50;
+
 
     switch (event.type) {
       case EventNames.PointerDown:
+        eventStarted = true;
+        console.log("event started: " + eventStarted);
         (<Element>event.target).setPointerCapture(event.pointerId);
         this._path = document.createElementNS("http://www.w3.org/2000/svg", "path");
         this._pathD = "M" + currentPoint.x + " " + currentPoint.y;
@@ -37,40 +46,96 @@ export class DrawPathTool implements ITool {
         this._path.setAttribute("stroke", designerCanvas.serviceContainer.globalContext.strokeColor);
         this._path.setAttribute("fill", designerCanvas.serviceContainer.globalContext.fillBrush);
         designerCanvas.overlayLayer.addOverlay(this._path, OverlayLayer.Foregorund);
+
+        if (lastPoint.x === currentPoint.x && lastPoint.y == currentPoint.y) {
+          samePoint = true;
+          console.log("Same Point: " + samePoint);
+        }
+
+        lastPoint = currentPoint;
         break;
 
       case EventNames.PointerMove:
-        if (this._path) {
-          this._pathD += "L" + currentPoint.x + " " + currentPoint.y;
-          this._path.setAttribute("d", this._pathD);
+        if (eventStarted) {
+          pointerMoved = true;
+          console.log("pointer moved: " + pointerMoved);
+        }
+        if (!p2pMode) {
+          if (this._path) {
+            this._pathD += "L" + currentPoint.x + " " + currentPoint.y;
+            this._path.setAttribute("d", this._pathD);
+          }
         }
         break;
 
       case EventNames.PointerUp:
         (<Element>event.target).releasePointerCapture(event.pointerId);
-        const rect = this._path.getBoundingClientRect();
+        if (eventStarted && !pointerMoved) {
+          p2pMode = true;
+          console.log("p2pMode" + p2pMode);
+        }
+        if (p2pMode) {
+          this._pathD += "L" + currentPoint.x + " " + currentPoint.y;
+          this._path.setAttribute("d", this._pathD);
+        }
+        else {
+          // const rect = this._path.getBoundingClientRect();
 
-        designerCanvas.overlayLayer.removeOverlay(this._path);
-        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+          // designerCanvas.overlayLayer.removeOverlay(this._path);
+          // const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 
-        const mvX = rect.x - designerCanvas.containerBoundingRect.x - offset;
-        const mvY = rect.y - designerCanvas.containerBoundingRect.y - offset;
-        const d = movePathData(this._path, mvX, mvY);
-        this._path.setAttribute("d", d);
-        svg.appendChild(this._path);
-        svg.style.left = (mvX) + 'px';
-        svg.style.top = (mvY) + 'px';
-        svg.style.position = 'absolute';
-        svg.style.width = (rect.width + 2 * offset) + 'px';
-        svg.style.height = (rect.height + 2 * offset) + 'px';
-        //designerView.rootDesignItem.element.appendChild(svg);
-        this._path = null;
-        this._pathD = null;
+          // const mvX = rect.x - designerCanvas.containerBoundingRect.x - offset;
+          // const mvY = rect.y - designerCanvas.containerBoundingRect.y - offset;
+          // const d = movePathData(this._path, mvX, mvY);
+          // this._path.setAttribute("d", d);
+          // svg.appendChild(this._path);
+          // svg.style.left = (mvX) + 'px';
+          // svg.style.top = (mvY) + 'px';
+          // svg.style.position = 'absolute';
+          // svg.style.width = (rect.width + 2 * offset) + 'px';
+          // svg.style.height = (rect.height + 2 * offset) + 'px';
+          // //designerView.rootDesignItem.element.appendChild(svg);
+          // this._path = null;
+          // this._pathD = null;
 
-        const di = DesignItem.createDesignItemFromInstance(svg, designerCanvas.serviceContainer, designerCanvas.instanceServiceContainer);
-        designerCanvas.instanceServiceContainer.undoService.execute(new InsertAction(designerCanvas.rootDesignItem, designerCanvas.rootDesignItem.childCount, di));
+          // const di = DesignItem.createDesignItemFromInstance(svg, designerCanvas.serviceContainer, designerCanvas.instanceServiceContainer);
+          // designerCanvas.instanceServiceContainer.undoService.execute(new InsertAction(designerCanvas.rootDesignItem, designerCanvas.rootDesignItem.childCount, di));
 
-        designerCanvas.serviceContainer.globalContext.finishedWithTool(this);
+        }
+        if (samePoint && p2pMode) {
+          eventStarted = false;
+          p2pMode = false;
+          pointerMoved = false;
+          samePoint = false;
+          console.log("event started: " + eventStarted);
+          console.log("p2pMode: " + p2pMode);
+          console.log("pointer moved: " + pointerMoved);
+          console.log("same point: " + samePoint);
+
+          const rect = this._path.getBoundingClientRect();
+
+          designerCanvas.overlayLayer.removeOverlay(this._path);
+          const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+          const mvX = rect.x - designerCanvas.containerBoundingRect.x - offset;
+          const mvY = rect.y - designerCanvas.containerBoundingRect.y - offset;
+          const d = movePathData(this._path, mvX, mvY);
+          this._path.setAttribute("d", d);
+          svg.appendChild(this._path);
+          svg.style.left = (mvX) + 'px';
+          svg.style.top = (mvY) + 'px';
+          svg.style.position = 'absolute';
+          svg.style.width = (rect.width + 2 * offset) + 'px';
+          svg.style.height = (rect.height + 2 * offset) + 'px';
+          //designerView.rootDesignItem.element.appendChild(svg);
+          this._path = null;
+          this._pathD = null;
+
+          const di = DesignItem.createDesignItemFromInstance(svg, designerCanvas.serviceContainer, designerCanvas.instanceServiceContainer);
+          designerCanvas.instanceServiceContainer.undoService.execute(new InsertAction(designerCanvas.rootDesignItem, designerCanvas.rootDesignItem.childCount, di));
+
+          designerCanvas.serviceContainer.globalContext.finishedWithTool(this);
+        }
         //TODO: Better Path drawing (like in SVGEDIT & Adding via Undo Framework. And adding to correct container)
 
         break;
