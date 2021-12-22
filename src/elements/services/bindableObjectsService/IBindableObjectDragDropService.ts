63ae4ab1911c27aba0ccb3b3e3fdada6a7de2354
jogibear9988 <jochen.kuehner@gmx.de>
2021-12-22 10:34:08 +0100

    work on bindings

--- src/elements/services/bindableObjectsService/IBindableObjectDragDropService.ts
+++ src/elements/services/bindableObjectsService/IBindableObjectDragDropService.ts
@@ -2,6 +2,6 @@ import { IDesignerCanvas } from "../../widgets/designerView/IDesignerCanvas.js";
 import { IBindableObject } from "./IBindableObject.js";
 
 export interface IBindableObjectDragDropService {
-  dragOver(event: DragEvent, bindableObject: IBindableObject): 'none' | 'copy' | 'link' | 'move';
-  drop(designerCanvas: IDesignerCanvas, event: DragEvent, bindableObject: IBindableObject);
+  dragOver(event: DragEvent, bindableObject: IBindableObject<any>): 'none' | 'copy' | 'link' | 'move';
+  drop(designerCanvas: IDesignerCanvas, event: DragEvent, bindableObject: IBindableObject<any>);
 }
