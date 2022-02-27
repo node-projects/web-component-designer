216ac2615261ba63f0e2168989ded0d9bbffc137
jogibear9988 <jochen.kuehner@gmx.de>
2022-02-27 08:06:21 +0100

    work on transformed resize

--- src/elements/helper/TransformHelper.ts
+++ src/elements/helper/TransformHelper.ts
@@ -1,3 +1,5 @@
+import { IPoint } from "../../index.js";
+
 export function combineTransforms(helperElement: HTMLElement, element: HTMLElement, transform1: string, transform2: string) {
   if (transform1 == null || transform1 == '') {
     element.style.transform = transform2;
@@ -12,4 +14,13 @@ export function combineTransforms(helperElement: HTMLElement, element: HTMLEleme
   const matrix2 = new DOMMatrix(window.getComputedStyle(helperElement).transform);
   const result = matrix2.multiply(matrix1);
   element.style.transform = result.toString();
+}
+
+export function getDomMatrix(element: HTMLElement) {
+  return new DOMMatrix(window.getComputedStyle(element).transform);
+}
+
+export function convertCoordinates(point: IPoint, matrix: DOMMatrix) {
+  let domPoint = new DOMPoint(point.x, point.y);
+  return domPoint.matrixTransform(matrix.inverse());
 }
