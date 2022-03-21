fc3b118c7de8785b7c83c1de626f9cd1e79651ad
Sven Knoerzer <sven.knoerzer@kardex.com>
2022-03-21 11:20:34 +0100

    refs #90 Draw Resize Overlay around transformed objects > Adjust resizer overlay for rotated element

--- src/elements/widgets/designerView/extensions/MouseOverExtension.ts
+++ src/elements/widgets/designerView/extensions/MouseOverExtension.ts
@@ -1,3 +1,5 @@
+import { IPoint3D } from "../../../../interfaces/IPoint3d";
+import { getTransformedCornerPoints } from "../../../helper/TransformHelper";
 import { IDesignItem } from "../../../item/IDesignItem";
 import { IDesignerCanvas } from "../IDesignerCanvas";
 import { AbstractExtension } from "./AbstractExtension";
@@ -7,7 +9,10 @@ const offset = 3;
 
 export class MouseOverExtension extends AbstractExtension {
 
-  private _rect: SVGRectElement;
+  private _line1: SVGLineElement;
+  private _line2: SVGLineElement;
+  private _line3: SVGLineElement;
+  private _line4: SVGLineElement;
 
   constructor(extensionManager: IExtensionManager, designerView: IDesignerCanvas, extendedItem: IDesignItem) {
     super(extensionManager, designerView, extendedItem);
@@ -18,12 +23,13 @@ export class MouseOverExtension extends AbstractExtension {
   }
 
   override refresh() {
-    let itemRect = this.extendedItem.element.getBoundingClientRect();
+    let clone = <HTMLElement>this.extendedItem.element.cloneNode();
+    let transformedCornerPoints: IPoint3D[] = getTransformedCornerPoints(clone, this.designerCanvas.helperElement, this.designerCanvas);
 
-    const xOffset = itemRect.x - this.designerCanvas.containerBoundingRect.x;
-    const yOffset = itemRect.y - this.designerCanvas.containerBoundingRect.y;
-
-    this._rect = this._drawRect((xOffset - offset) / this.designerCanvas.scaleFactor, (yOffset - offset) / this.designerCanvas.scaleFactor, (itemRect.width + offset + offset) / this.designerCanvas.scaleFactor, (itemRect.height + offset + offset) / this.designerCanvas.scaleFactor, 'svg-hover', this._rect);
+    this._line1 = this._drawLine(transformedCornerPoints[0].x - offset, transformedCornerPoints[0].y - offset, transformedCornerPoints[1].x + offset, transformedCornerPoints[1].y - offset, 'svg-hover', this._line1);
+    this._line2 = this._drawLine(transformedCornerPoints[0].x - offset, transformedCornerPoints[0].y - offset, transformedCornerPoints[2].x - offset, transformedCornerPoints[2].y + offset, 'svg-hover', this._line2);
+    this._line3 = this._drawLine(transformedCornerPoints[1].x + offset, transformedCornerPoints[1].y - offset, transformedCornerPoints[3].x + offset, transformedCornerPoints[3].y + offset, 'svg-hover', this._line3);
+    this._line4 = this._drawLine(transformedCornerPoints[2].x - offset, transformedCornerPoints[2].y + offset, transformedCornerPoints[3].x + offset, transformedCornerPoints[3].y + offset, 'svg-hover', this._line4);
   }
 
   override dispose() {
