750292274423f7851632b6b93f9cf7a715b4b8fe
Jochen Kühner <jochen.kuehner@gmx.de>
2022-05-05 23:29:23 +0200

    fix canvas extension

--- src/elements/widgets/designerView/extensions/CanvasExtension.ts
+++ src/elements/widgets/designerView/extensions/CanvasExtension.ts
@@ -10,21 +10,18 @@ export class CanvasExtension extends AbstractExtension {
   }
 
   override extend() {
-    let itemRect = this.extendedItem.element.getBoundingClientRect();
+    let itemRect = this.designerCanvas.getNormalizedElementCoordinates(this.extendedItem.element);
     const computedStyle = getComputedStyle(this.extendedItem.element);
     if (computedStyle.margin !== '0px') {
-      const xOffset = itemRect.x - this.designerCanvas.containerBoundingRect.x;
-      const yOffset = itemRect.y - this.designerCanvas.containerBoundingRect.y;
-
       const left = Number.parseFloat(computedStyle.marginLeft.replace('px', ''));
       const top = Number.parseFloat(computedStyle.marginTop.replace('px', ''));
       const right = Number.parseFloat(computedStyle.marginRight.replace('px', ''));
       const bottom = Number.parseFloat(computedStyle.marginBottom.replace('px', ''));
 
-      this._drawRect(xOffset - left, yOffset - top, left + itemRect.width + right, top, 'svg-margin');
-      this._drawRect(xOffset - left, yOffset, left, itemRect.height, 'svg-margin');
-      this._drawRect(xOffset + itemRect.width, yOffset, right, itemRect.height, 'svg-margin');
-      this._drawRect(xOffset - left, yOffset + itemRect.height, left + itemRect.width + right, bottom, 'svg-margin');
+      this._drawRect(itemRect.x - left, itemRect.y - top, left + itemRect.width + right, top, 'svg-margin');
+      this._drawRect(itemRect.x - left, itemRect.y, left, itemRect.height, 'svg-margin');
+      this._drawRect(itemRect.x + itemRect.width, itemRect.y, right, itemRect.height, 'svg-margin');
+      this._drawRect(itemRect.x - left, itemRect.y + itemRect.height, left + itemRect.width + right, bottom, 'svg-margin');
     }
   }
 
