72ef8c917c86e048ecd80b48d3b37a90310abf4a
jkuehner <jochen.kuehner@kardex.com>
2022-12-02 15:45:36 +0100

    fix more extensions

--- src/elements/widgets/designerView/extensions/InvisibleDivExtension.ts
+++ src/elements/widgets/designerView/extensions/InvisibleDivExtension.ts
@@ -1,3 +1,4 @@
+import { getDesignerCanvasNormalizedTransformedCornerDOMPoints } from "../../../helper/TransformHelper";
 import { IDesignItem } from "../../../item/IDesignItem";
 import { IDesignerCanvas } from "../IDesignerCanvas";
 import { AbstractExtension } from './AbstractExtension';
@@ -5,7 +6,7 @@ import { IExtensionManager } from "./IExtensionManger";
 import { OverlayLayer } from "./OverlayLayer.js";
 
 export class InvisibleDivExtension extends AbstractExtension {
-  private _rect: SVGRectElement;
+  private _rect: SVGPathElement;
 
   constructor(extensionManager: IExtensionManager, designerCanvas: IDesignerCanvas, extendedItem: IDesignItem) {
     super(extensionManager, designerCanvas, extendedItem);
@@ -16,8 +17,8 @@ export class InvisibleDivExtension extends AbstractExtension {
   }
 
   override refresh() {
-    const itemRect = this.designerCanvas.getNormalizedElementCoordinates(this.extendedItem.element);
-    this._rect = this._drawRect(itemRect.x, itemRect.y, itemRect.width, itemRect.height, 'svg-invisible-div', this._rect, OverlayLayer.Background);
+    let transformedCornerPoints = getDesignerCanvasNormalizedTransformedCornerDOMPoints(<HTMLElement>this.extendedItem.element, 0, this.designerCanvas);
+    this._rect = this._drawTransformedRect(transformedCornerPoints, 'svg-invisible-div', this._rect, OverlayLayer.Background);
   }
 
   override dispose() {
