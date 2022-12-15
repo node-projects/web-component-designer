603ccfffa4782b82292a41e63cd884c60aa15a13
jkuehner <jochen.kuehner@kardex.com>
2022-12-15 17:36:31 +0100

    possibility to set a filter

--- src/elements/widgets/designerView/extensions/InvisibleElementExtensionProvider.ts
+++ src/elements/widgets/designerView/extensions/InvisibleElementExtensionProvider.ts
@@ -9,9 +9,16 @@ import { css } from "@node-projects/base-custom-webcomponent";
 export const invisibleElementExtensionShowOverlayOptionName = 'invisibleElementExtensionShowOverlay';
 
 export class InvisibleElementExtensionProvider implements IDesignerExtensionProvider {
+
+  elementFilter: (designItem: IDesignItem) => boolean;
+
+  constructor(elementFilter: (designItem: IDesignItem) => boolean = (d) => d.name == 'div' && window.getComputedStyle(d.element).display != 'inline') {
+    this.elementFilter = elementFilter;
+  }
+
   shouldExtend(extensionManager: IExtensionManager, designerCanvas: IDesignerCanvas, designItem: IDesignItem): boolean {
     if (designerCanvas.instanceServiceContainer.designContext.extensionOptions[invisibleElementExtensionShowOverlayOptionName] !== false) {
-      if (designItem.name == 'div') {
+      if (this.elementFilter(designItem)) {
         const st = window.getComputedStyle(designItem.element);
         return st.backgroundColor == 'rgba(0, 0, 0, 0)' && st.borderStyle == 'none'
       }
