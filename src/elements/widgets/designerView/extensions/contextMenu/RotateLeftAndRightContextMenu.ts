b860749a1d71da7f054c20a5716e1dcd5dc0567e
jkuehner <jochen.kuehner@kardex.com>
2023-01-31 06:56:42 +0100

    contextmenus only for element nodes

--- src/elements/widgets/designerView/extensions/contextMenu/RotateLeftAndRightContextMenu.ts
+++ src/elements/widgets/designerView/extensions/contextMenu/RotateLeftAndRightContextMenu.ts
@@ -1,13 +1,14 @@
 import { CommandType } from '../../../../../commandHandling/CommandType.js';
 import { IContextMenuItem } from '../../../../helper/contextMenu/IContextMenuItem.js';
 import { IDesignItem } from '../../../../item/IDesignItem.js';
+import { NodeType } from '../../../../item/NodeType.js';
 import { IDesignerCanvas } from '../../IDesignerCanvas.js';
 import { ContextmenuInitiator, IContextMenuExtension } from './IContextMenuExtension.js';
 
 export class RotateLeftAndRight implements IContextMenuExtension{
 
   public shouldProvideContextmenu(event: MouseEvent, designerView: IDesignerCanvas, designItem: IDesignItem, initiator: ContextmenuInitiator) {
-    return !designItem.isRootItem;
+    return !designItem.isRootItem && designItem.nodeType == NodeType.Element;
   }
 
   public provideContextMenuItems(event: MouseEvent, designerView: IDesignerCanvas, designItem: IDesignItem): IContextMenuItem[] {
