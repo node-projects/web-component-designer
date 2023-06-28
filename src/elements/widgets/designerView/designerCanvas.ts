93e37f4c420e646c93943e35baf82e105080db17
kai kranich <kai.kranich@kardex.com>
2023-06-28 07:47:51 +0200

    improved last changes by using "instance of SVGElement" instead checking nodeName and parent.nodeName

--- src/elements/widgets/designerView/designerCanvas.ts
+++ src/elements/widgets/designerView/designerCanvas.ts
@@ -864,9 +864,7 @@ export class DesignerCanvas extends BaseCustomWebComponentLazyAppend implements 
         const containerStyle = getComputedStyle(newContainerElementDesignItem.element);
         newContainerService = this.serviceContainer.getLastServiceWhere('containerService', x => x.serviceForContainer(newContainerElementDesignItem, containerStyle));
         if (newContainerService) {
-          if (newContainerService.canEnterByDrop(newContainerElementDesignItem) &&
-            newContainerElementDesignItem.element.nodeName != 'svg' &&
-            newContainerElementDesignItem.parent.element.nodeName != 'svg') {
+          if (newContainerService.canEnterByDrop(newContainerElementDesignItem) && !(newContainerElementDesignItem.element instanceof SVGElement)) {
             break;
           } else {
             newContainerElementDesignItem = null;
