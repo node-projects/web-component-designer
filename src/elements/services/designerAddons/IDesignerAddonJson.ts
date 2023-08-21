8e38ca93a52a6aaa3a36907085a8462e12a14b98
jkuehner <jochen.kuehner@kardex.com>
2023-08-21 07:25:09 +0200

    remove unused

--- src/elements/services/designerAddons/IDesignerAddonJson.ts
+++ src/elements/services/designerAddons/IDesignerAddonJson.ts
@@ -1,6 +1,3 @@
 export interface IDesignerAddonJson {
-    services: Record<string, string>,
-    components: Record<string, {
-        stylesheets: string[]
-    }>
+    services: Record<string, string>
 }
