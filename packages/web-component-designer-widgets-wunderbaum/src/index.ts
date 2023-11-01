73013cc674781e082162416cd38113faf272379b
jogibear9988 <jochen.kuehner@gmx.de>
2023-11-01 08:39:13 +0100

    fix wunderbaum selection

--- packages/web-component-designer-widgets-wunderbaum/src/index.ts
+++ packages/web-component-designer-widgets-wunderbaum/src/index.ts
@@ -1,4 +1,4 @@
 export * from "./widgets/bindableObjectsBrowser/bindable-objects-browser.js";
 export * from "./widgets/paletteView/paletteTreeView.js";
 export * from "./widgets/treeView/treeViewExtended.js";
-export * as DefaultWunderbaumOptions from "./widgets/WunderbaumOptions.js";
+export * from "./widgets/WunderbaumOptions.js";
