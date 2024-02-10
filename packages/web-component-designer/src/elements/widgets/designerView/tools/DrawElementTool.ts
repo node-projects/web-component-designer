cfbff4d5bcff3e5babd83057dbacaabf72cfdc51
jogibear9988 <jochen.kuehner@gmx.de>
2024-02-10 20:22:02 +0100

    fix draw tool

--- packages/web-component-designer/src/elements/widgets/designerView/tools/DrawElementTool.ts
+++ packages/web-component-designer/src/elements/widgets/designerView/tools/DrawElementTool.ts
@@ -3,7 +3,7 @@ import { IPoint } from '../../../../interfaces/IPoint.js';
 import { IDesignItem } from '../../../item/IDesignItem.js';
 import { IElementDefinition } from '../../../services/elementsService/IElementDefinition.js';
 import { ServiceContainer } from '../../../services/ServiceContainer.js';
-import { InsertAction } from '../../../services/undoService/transactionItems/InsertAction.js';
+import { ChangeGroup } from '../../../services/undoService/ChangeGroup.js';
 import { OverlayLayer } from '../extensions/OverlayLayer.js';
 import { IDesignerCanvas } from '../IDesignerCanvas.js';
 import { ITool } from './ITool.js';
@@ -12,6 +12,7 @@ export class DrawElementTool implements ITool {
   private _elementDefinition: IElementDefinition;
   private _createdItem: IDesignItem;
   private _startPosition: IPoint;
+  private _changeGroup: ChangeGroup;
 
   readonly cursor = 'crosshair';
   private _rect: any;
@@ -42,13 +43,14 @@ export class DrawElementTool implements ITool {
     }
   }
 
-  keyboardEventHandler(designerCanvas: IDesignerCanvas, event: KeyboardEvent, currentElement: Element) 
-  { }
+  keyboardEventHandler(designerCanvas: IDesignerCanvas, event: KeyboardEvent, currentElement: Element) { }
 
+  sizeOverlapThreshold = false;
   private async _onPointerDown(designerView: IDesignerCanvas, event: PointerEvent) {
     event.preventDefault();
     this._startPosition = { x: event.x, y: event.y };
 
+    this._changeGroup = designerView.rootDesignItem.openGroup("Insert Item");
     this._createdItem = await designerView.serviceContainer.forSomeServicesTillResult("instanceService", (service) => service.getElement(this._elementDefinition, designerView.serviceContainer, designerView.instanceServiceContainer));
     const targetRect = (<HTMLElement>event.target).getBoundingClientRect();
     this._createdItem.setStyle('position', 'absolute');
@@ -59,17 +61,14 @@ export class DrawElementTool implements ITool {
     this._createdItem.setStyle('height', '0');
     (<HTMLElement>this._createdItem.element).style.overflow = 'hidden';
 
-    //TODO: add items as last, with all properties set
+    designerView.rootDesignItem.insertChild(this._createdItem);
     //draw via containerService??? how to draw into a grid, a stackpanel???
-    designerView.instanceServiceContainer.undoService.execute(new InsertAction(designerView.rootDesignItem, designerView.rootDesignItem.childCount, this._createdItem));
-
     designerView.instanceServiceContainer.selectionService.clearSelectedElements();
   }
 
   private async _onPointerMove(designerCanvas: IDesignerCanvas, event: PointerEvent) {
     if (this._createdItem) {
       if (!this._rect) {
-        designerCanvas.rootDesignItem.element.appendChild(this._createdItem.element);
         this._rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
         designerCanvas.overlayLayer.addOverlay(this.constructor.name, this._rect, OverlayLayer.Foreground);
         this._rect.setAttribute('class', 'svg-draw-new-element');
@@ -87,12 +86,20 @@ export class DrawElementTool implements ITool {
         this._rect.setAttribute('height', h);
         this._createdItem.setStyle('height', h + 'px');
       }
+
+      if (w > 5 || h > 5)
+        this.sizeOverlapThreshold = true;
     }
   }
 
   private async _onPointerUp(designerView: IDesignerCanvas, event: PointerEvent) {
+    if (this.sizeOverlapThreshold) {
+      this._changeGroup.commit();
+      designerView.instanceServiceContainer.selectionService.setSelectedElements([this._createdItem]);
+    } else {
+      this._changeGroup.abort();
+    }
     designerView.overlayLayer.removeOverlay(this._rect);
-    designerView.instanceServiceContainer.selectionService.setSelectedElements([this._createdItem]);
     this._startPosition = null;
     this._rect = null;
     this._createdItem = null;
