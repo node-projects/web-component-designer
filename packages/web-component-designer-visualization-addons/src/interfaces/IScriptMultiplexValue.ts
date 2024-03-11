eb99404c068091f3f35bbdf8ef3d24ecefc7d35b
jogibear9988 <jochen.kuehner@gmx.de>
2024-03-11 20:15:54 +0100

    wrong typing

--- packages/web-component-designer-visualization-addons/src/interfaces/IScriptMultiplexValue.ts
+++ packages/web-component-designer-visualization-addons/src/interfaces/IScriptMultiplexValue.ts
@@ -3,7 +3,6 @@ export class IScriptMultiplexValue {
      * signal - read the value from a IOB object
      * property - read the value from a property of the customControl (not usable in screens)
      * event - read the value of a property of the event object
-     * @TJS-format signal
      */
     source: 'signal' | 'property' | 'event';
     /**
