666fc58ad6fb117e4c5787c6996a17a02cd45398
jogibear9988 <jochen.kuehner@gmx.de>
2024-03-26 06:54:52 +0100

    fix null ref in extension

--- packages/web-component-designer/src/elements/widgets/designerView/extensions/PositionExtensionProvider.ts
+++ packages/web-component-designer/src/elements/widgets/designerView/extensions/PositionExtensionProvider.ts
@@ -8,13 +8,15 @@ import { css } from "@node-projects/base-custom-webcomponent";
 
 export class PositionExtensionProvider implements IDesignerExtensionProvider {
   shouldExtend(extensionManager: IExtensionManager, designerView: IDesignerCanvas, designItem: IDesignItem): boolean {
+    if (!designItem?.parent)
+      return false;
     const cs = getComputedStyle((<HTMLElement>designItem.element));
     if (cs.position === 'relative' || cs.position === 'absolute')
       return true;
     return false;
   }
 
-  getExtension(extensionManager: IExtensionManager, designerView: IDesignerCanvas,  designItem: IDesignItem): IDesignerExtension {
+  getExtension(extensionManager: IExtensionManager, designerView: IDesignerCanvas, designItem: IDesignItem): IDesignerExtension {
     return new PositionExtension(extensionManager, designerView, designItem);
   }
 
