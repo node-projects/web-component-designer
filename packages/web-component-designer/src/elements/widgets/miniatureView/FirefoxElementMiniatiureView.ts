ee404b90ccc89478e7f1dd5fccbd2272ef804a96
jogibear9988 <jochen.kuehner@gmx.de>
2025-05-23 15:17:36 +0200

    wip firefox miniatur view

---
+++ packages/web-component-designer/src/elements/widgets/miniatureView/FirefoxElementMiniatiureView.ts
@@ -0,0 +1,26 @@
+import { BaseCustomWebComponentLazyAppend, html } from '@node-projects/base-custom-webcomponent';
+import { IDesignerCanvas, IMiniatureView } from '@node-projects/web-component-designer';
+
+//TODO: does not work atm.
+//see: https://bugzilla.mozilla.org/show_bug.cgi?id=1966844
+export class FirefoxElementMiniatiureView extends BaseCustomWebComponentLazyAppend implements IMiniatureView {
+
+  static override readonly template = html`<div class="imgdiv"></div>`;
+
+  private _imgdiv: HTMLDivElement;
+
+  constructor() {
+    super();
+    this._restoreCachedInititalValues();
+    
+    this._imgdiv = this._getDomElement<HTMLDivElement>('imgdiv');
+    this._imgdiv.style.backgroundImage = `-moz-element(#web-compoent-designer-${1234})`
+  }
+
+  reRender(designerView: IDesignerCanvas) {
+    //@ts-ignore
+    document.mozSetImageElement("web-compoent-designer", designerView.rootDesignItem.element);
+  }
+}
+
+customElements.define('node-projects-firefox-element-miniature-view', FirefoxElementMiniatiureView);
