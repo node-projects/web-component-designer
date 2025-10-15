db522f535320dd3bd8fb50f7e5fa7997c23515f7
jogibear9988 <jochen.kuehner@gmx.de>
2025-10-15 14:16:20 +0200

    getLastServiceResult

--- packages/web-component-designer/src/elements/services/BaseServiceContainer.ts
+++ packages/web-component-designer/src/elements/services/BaseServiceContainer.ts
@@ -68,6 +68,20 @@ export class BaseServiceContainer<NameMap> {
     return null;
   }
 
+  getLastServiceResult<K extends keyof NameMap, Y>(service: K, callback: (service: NameMap[K]) => Y): Y {
+    let services = this.getServices<K>(<any>service);
+    if (services == null) {
+      return null;
+    }
+    for (let index = services.length - 1; index >= 0; index--) {
+      const currentService = services[index];
+      let result = callback(currentService);
+      if (result)
+        return result;
+    }
+    return null;
+  }
+
   async getLastServiceWhereAsync<K extends keyof NameMap, Y>(service: K, callback: (service: NameMap[K]) => Promise<Y>): Promise<NameMap[K]> {
     let services = this.getServices<K>(<any>service);
     if (services == null) {
