fd1500c2d8f6329b5b12c85c6be5d4a317718a0c
jogibear9988 <jochen.kuehner@gmx.de>
2025-11-19 19:41:29 +0100

    fix position extension

--- packages/web-component-designer/src/elements/widgets/designerView/extensions/PositionExtension.ts
+++ packages/web-component-designer/src/elements/widgets/designerView/extensions/PositionExtension.ts
@@ -23,7 +23,7 @@ export class PositionExtension extends AbstractExtension {
   override refresh() {
 
     //TODO: show wich position is docked (no dashed line, but full). klick on value disables or enables docking? Disabling enables parent side, enabling both removes width or height.
-    
+
     const itemRect = this.designerCanvas.getNormalizedElementCoordinates(this.extendedItem.element);
     const itemParentRect = this.designerCanvas.getNormalizedElementCoordinates(this.extendedItem.parent.element);
 
@@ -41,14 +41,30 @@ export class PositionExtension extends AbstractExtension {
       this._line3.style.strokeDasharray = '' + (4 / this.designerCanvas.scaleFactor);
       this._line4.style.strokeDasharray = '' + (4 / this.designerCanvas.scaleFactor);
 
-      let x = Math.round(itemRect.x - itemParentRect.x);
-      let y = Math.round(itemRect.y - itemParentRect.y);
-      this._textX = this._drawTextWithBackground('' + x, itemParentRect.x + x / 2, itemRect.y + itemRect.height / 2, 'white', 'svg-position-text', this._textX);
-      this._textX[2].style.fontSize = (12 / this.designerCanvas.scaleFactor) + 'px';
-      this._textX[3].style.fontSize = (12 / this.designerCanvas.scaleFactor) + 'px';
-      this._textY = this._drawTextWithBackground('' + y, itemRect.x + itemRect.width / 2, itemParentRect.y + y / 2, 'white', 'svg-position-text', this._textY);
-      this._textY[2].style.fontSize = (12 / this.designerCanvas.scaleFactor) + 'px';
-      this._textY[3].style.fontSize = (12 / this.designerCanvas.scaleFactor) + 'px';
+      const cm = this.extendedItem.element.computedStyleMap();
+      if (!(cm.get('right') instanceof CSSUnitValue)) {
+        let x = Math.round(itemRect.x - itemParentRect.x);
+        this._textX = this._drawTextWithBackground('' + x, itemParentRect.x + x / 2, itemRect.y + itemRect.height / 2, 'white', 'svg-position-text', this._textX);
+        this._textX[2].style.fontSize = (12 / this.designerCanvas.scaleFactor) + 'px';
+        this._textX[3].style.fontSize = (12 / this.designerCanvas.scaleFactor) + 'px';
+      } else {
+        let x = Math.round(itemParentRect.x + itemParentRect.width - (itemRect.x + itemRect.width));
+        this._textX = this._drawTextWithBackground('' + x, itemParentRect.x + itemParentRect.width - x / 2, itemRect.y + itemRect.height / 2, 'white', 'svg-position-text', this._textX);
+        this._textX[2].style.fontSize = (12 / this.designerCanvas.scaleFactor) + 'px';
+        this._textX[3].style.fontSize = (12 / this.designerCanvas.scaleFactor) + 'px';
+      }
+
+      if (!(cm.get('bottom') instanceof CSSUnitValue)) {
+        let y = Math.round(itemRect.y - itemParentRect.y);
+        this._textY = this._drawTextWithBackground('' + y, itemRect.x + itemRect.width / 2, itemParentRect.y + y / 2, 'white', 'svg-position-text', this._textY);
+        this._textY[2].style.fontSize = (12 / this.designerCanvas.scaleFactor) + 'px';
+        this._textY[3].style.fontSize = (12 / this.designerCanvas.scaleFactor) + 'px';
+      } else {
+        let y = Math.round(itemParentRect.y + itemParentRect.height - (itemRect.y + itemRect.height));
+        this._textY = this._drawTextWithBackground('' + y, itemRect.x + itemRect.width / 2, itemParentRect.y + itemParentRect.height - y / 2, 'white', 'svg-position-text', this._textY);
+        this._textY[2].style.fontSize = (12 / this.designerCanvas.scaleFactor) + 'px';
+        this._textY[3].style.fontSize = (12 / this.designerCanvas.scaleFactor) + 'px';
+      }
     }
   }
 
